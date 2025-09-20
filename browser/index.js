1c1a36bd100f038e098d1214ff939dec370feefc
Isaac Chacko <isaac.chacko05@gmail.com>
2025-09-20 09:06:36 -0500

    Revert "DELETED BROWSER"
    This reverts commit 48bbdda0823abff1d9c5c616954d404385808b1d.

---
+++ browser/index.js
@@ -0,0 +1,33 @@
+
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      webviewTag: true,
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+// allow for localhost usage
+app.commandLine.appendSwitch('--allow-running-insecure-content')
+app.commandLine.appendSwitch('--allow-insecure-localhost', 'true')
+app.commandLine.appendSwitch('--ignore-ssl-errors')
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
---
+++ browser/renderer.js
@@ -0,0 +1,48 @@
+
+const webview = document.getElementById('webview')
+const urlInput = document.getElementById('url-input')
+const backBtn = document.getElementById('back-btn')
+const forwardBtn = document.getElementById('forward-btn')
+const reloadBtn = document.getElementById('reload-btn')
+const goBtn = document.getElementById('go-btn')
+
+// Navigation functions
+backBtn.addEventListener('click', () => {
+  if (webview.canGoBack()) {
+    webview.goBack()
+  }
+})
+
+forwardBtn.addEventListener('click', () => {
+  if (webview.canGoForward()) {
+    webview.goForward()
+  }
+})
+
+reloadBtn.addEventListener('click', () => {
+  webview.reload()
+})
+
+goBtn.addEventListener('click', () => {
+  let url = urlInput.value
+  if (!url.startsWith('http://') && !url.startsWith('https://')) {
+    url = 'https://' + url
+  }
+  webview.src = url
+})
+
+// Update URL bar when navigation occurs
+webview.addEventListener('did-navigate', (event) => {
+  urlInput.value = event.url
+})
+
+webview.addEventListener('did-navigate-in-page', (event) => {
+  urlInput.value = event.url
+})
+
+// Handle Enter key in URL bar
+urlInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    goBtn.click()
+  }
+})
